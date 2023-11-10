5dee34c8d377e6b832e8396372c93424a6b90328
jasingizwe <c.asingizwe@alustudent.com>
2023-11-10 10:07:25 -0800

    done

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,13 +3,15 @@ def to_subtract(list_num):
     to_sub = 0
     max_list = max(list_num) 
     for n in list_num:
-        if max_list > n:
+     if max_list > n:
             to_sub += n
     return (max_list - to_sub)
+
+
 def roman_to_int(roman_string):
-    if not roman_string:    
+ if not roman_string:    
         return 0
-    if not isinstance(roman_string, str):    
+ if not isinstance(roman_string, str):    
         return 0
     rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     list_keys = list(rom_n.keys())
@@ -23,7 +25,6 @@ def roman_to_int(roman_string):
                     num += to_subtract(list_num)
                     list_num = [rom_n.get(ch)]
                 else:
-                    list_num.append(rom_n.get(ch))      
-                last_rom = rom_n.get(ch)
+                    list_num.append(rom_n.get(ch))  last_rom = rom_n.get(ch)
     num += to_subtract(list_num)
     return (num)
