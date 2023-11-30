4f123ab2a990f8b76128f36b1c2642ddf7883a3e
jasingizwe <c.asingizwe@alustudent.com>
2023-11-30 02:28:54 -0800

    more

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,12 @@
--- list privileges
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- 0-privileges.sql
+
+-- Create users
+CREATE USER 'user_0d_1'@'localhost';
+CREATE USER 'user_0d_2'@'localhost';
+
+-- Grant privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Grant privileges to user_0d_2
+GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'user_0d_2'@'localhost';
+
