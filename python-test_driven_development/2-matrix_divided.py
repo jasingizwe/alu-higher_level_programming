557e959c7e612aa8a3e87b60f0235f25dafaf9dc
jasingizwe <c.asingizwe@alustudent.com>
2023-12-10 01:23:44 -0800

    now

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""divide matrix module"""
+
+
+def matrix_divided(matrix, div):
+    """divides a matrix elements with a number"""
+    if type(div) != int and type(div) != float:
+        raise TypeError('div must be a number')
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError('Each row of the matrix must have the same size')
+    try:
+        return [[round(j / div, 2) for j in i] for i in matrix]
+    except TypeError:
+        message = 'matrix must be a matrix (list of lists) of integers/floats'
+        raise TypeError(message)
+    except ZeroDivisionError:
+        raise ZeroDivisionError('division by zero')
