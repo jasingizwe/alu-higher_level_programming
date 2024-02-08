c684ec67713b9c04cabc8e9b9d12aa34c7892779
jasingizwe <c.asingizwe@alustudent.com>
2024-02-08 06:16:37 -0800

    scraping

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,29 +3,4 @@ const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], (err, data) => {
   if (err) console.log(err);
-});#!/usr/bin/node
-const fs = require('fs');
-
-fs.writeFile(process.argv[2], process.argv[3], (err, data) => {
-	  if (err) console.log(err);
-});#!/usr/bin/node
-const fs = require('fs');
-
-fs.writeFile(process.argv[2], process.argv[3], (err, data) => {
-	  if (err) console.log(err);
-});#!/usr/bin/node
-const fs = require('fs');
-
-fs.writeFile(process.argv[2], process.argv[3], (err, data) => {
-	  if (err) console.log(err);
-});#!/usr/bin/node
-const fs = require('fs');
-
-fs.writeFile(process.argv[2], process.argv[3], (err, data) => {
-	  if (err) console.log(err);
-});#!/usr/bin/node
-const fs = require('fs');
-
-fs.writeFile(process.argv[2], process.argv[3], (err, data) => {
-	  if (err) console.log(err);
 });
